51513f69f592d35af32e49662e94e52ace10457e
Angeli <angeli.mittal@gmail.com>
2022-09-08 11:36:59 -0500

    finish up to half of resume, remove unused css code

--- HTML/mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'Ober - CV Resume Template'"; //letter subject
-	$to = 'youremail@domain.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
-	$charset = 'UTF-8';
-?>
